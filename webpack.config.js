5e68f630196848a91df7fe61c1ed276d501b8c89
Florent Catiau-Tristant <florent.ct@orange.fr>
2020-02-19 19:58:02 +0100

    chore: update licence

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/ts-matrix.ts',
   devtool: 'inline-source-map',
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
